b6f9475984077d3f4e2fa5572c78d2624994cf27
AntonykPetr89 <petrantoniuc@yandex.com>
2024-09-23 09:34:42 +0300

    исправление hw2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
@@ -10,48 +10,44 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.UserType.Type.*;
 
-@ExtendWith(BrowserExtension.class)
+@ExtendWith({BrowserExtension.class, UsersQueueExtension.class})
 
 public class FriendsWebTest {
     private static final Config CFG = Config.getInstance();
 
 
     @Test
-    @ExtendWith(UsersQueueExtension.class)
     void friendShouldBePresentInFriendsTable(@UsersQueueExtension.UserType(WITH_FRIEND) UsersQueueExtension.StaticUser user) {
         Selenide.open(CFG.frontUrl(), LoginPage.class)
-                .login("Ivan", "A97f092a")
+                .login(user.username(), user.password())
                 .navigateToFriends()
                 .shouldBeHeaderInFriendTable("My friends")
-                .shouldBeFriendInTable("Nikolai");
+                .shouldBeFriendInTable(user.friend());
     }
 
     @Test
-    @ExtendWith(UsersQueueExtension.class)
     public void friendsTableShouldBeEmptyForNewUser(@UsersQueueExtension.UserType(EMPTY) UsersQueueExtension.StaticUser user) {
         Selenide.open(CFG.frontUrl(), LoginPage.class)
-                .login("Petr", "A97f092a")
+                .login(user.username(), user.password())
                 .navigateToFriends()
                 .checkMessageInEmptyTable();
     }
 
     @Test
-    @ExtendWith(UsersQueueExtension.class)
     public void incomeInvitationBePresentInFriendsTable(@UsersQueueExtension.UserType(WITH_INCOME_REQUEST) UsersQueueExtension.StaticUser user) {
         Selenide.open(CFG.frontUrl(), LoginPage.class)
-                .login("Vasya", "A97f092a")
+                .login(user.username(), user.password())
                 .navigateToFriends()
                 .shouldBeHeaderForIncomeFriendRequest("Friend requests")
-                .shouldBeFriendRequestInTable("Max");
+                .shouldBeFriendRequestInTable(user.income());
     }
 
     @Test
-    @ExtendWith(UsersQueueExtension.class)
     public void outcomeInvitationBePresentInAllPeoplesTable(@UsersQueueExtension.UserType(WITH_OUTCOME_REQUEST) UsersQueueExtension.StaticUser user) {
         Selenide.open(CFG.frontUrl(), LoginPage.class)
-                .login("Max", "A97f092a")
+                .login(user.username(), user.password())
                 .navigateToAllPeople()
-                .shouldBePresentOutcomeRequestInAllPeopleTable("Vasya", "Waiting...");
+                .shouldBePresentOutcomeRequestInAllPeopleTable(user.outcome(), "Waiting...");
     }
 
 }
